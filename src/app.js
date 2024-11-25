41c1b854fbe750e7d17bbf0d9f3f4500cf166be6
HrishikeshPK <hrishilovjith@gmail.com>
2024-11-25 22:29:27 +0530

    CRUD API tested

--- src/app.js
+++ src/app.js
@@ -3,14 +3,11 @@ const connectDB = require("./config/database")
 const app = express()
 const User = require("./models/user")
 
+app.use(express.json())  // middleware provided by express which is activated(.use) for all cases (routes)
+
 app.post("/signup", async(req, res) => {
     // Creating a new instance of the User model
-    const user = new User({
-        firstName: "Kashi",
-        lastName: "Nath",
-        emailId: "pkpruam@pk.com",
-        password: "nathan",
-    })
+    const user = new User(req.body)
 
     try{
         await user.save()
@@ -19,6 +16,60 @@ app.post("/signup", async(req, res) => {
         res.status(400).send("Error saving the user:"+ err.message)
     }
     
+})
+
+
+// Get user by email
+app.get("/user", async (req,res)=>{
+    const userEmail = req.body.emailId
+
+    try {
+        const user = await User.find({emailId: userEmail})
+        if(!user){
+            res.status(404).send("User not found")
+        }else {
+            res.send(user)
+        }
+    } catch {
+        res.status(400).send("Error saving the user:"+ err.message)
+
+    }
+})
+
+
+// Feed API - GET/feed - get all the users from the database
+app.get("/feed", async (req, res)=>{
+    try{
+       const users = await User.find({})
+       res.send(users)
+    }catch (err) {
+        res.status(400).send("Something went wrong")
+    }
+})
+
+// delete user API 
+app.delete("/user", async (req, res)=> {
+    const userId = req.body.userId
+
+    try {
+        const users = await User.findByIdAndDelete(userId)
+        res.send("User deleted successfully")
+    }catch (err) {
+        res.status(400).send("Something went wrong")
+    }
+})
+
+// update API
+app.patch("/user", async(req, res) => {
+    const userId = req.body.userId
+    const data = req.body
+
+    try {
+        await User.findByIdAndUpdate({_id: userId}, data )
+        res.send("User details updated")
+    }catch (err) {
+        res.status(400).send("Something went wrong")
+    }
 })
 
 connectDB()
@@ -35,4 +86,4 @@ connectDB()
 
 
 
-// order of the routes mater a lot
+// order of the routes matter a lot
