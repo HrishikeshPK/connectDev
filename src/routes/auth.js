45364f888dfc7383604102bf90e93d300821828c
HrishikeshPK <hrishilovjith@gmail.com>
2025-01-01 12:47:06 +0530

    wed2

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -30,10 +30,13 @@ authRouter.post("/signup", async(req, res) => {
         const savedUser = await user.save()
         const token = await savedUser.getJWT()
 
+        // res.cookie("token", token, {
+        //     expires: new Date(Date.now() + 8 * 3600000)
+        // })
         res.cookie("token", token, {
             httpOnly: true,
-            secure: true,
             sameSite: "None",
+            secure: process.env.NODE_ENV === "production",
             expires: new Date(Date.now() + 8 * 3600000)
         })
         
